c4717becd7091bb5e4f5e2e33b5045620b358840
daiwata <watanabedai5427@gmail.com>
2023-07-16 05:15:24 +0900

    mod detail total

--- src/conv_detail.py
+++ src/conv_detail.py
@@ -1,27 +1,41 @@
 from collections import defaultdict
 from collections import OrderedDict
-from datetime import datetime 
+
+from collections import defaultdict
 
 def detail_aggregate_data(data):
-    aggregated_data = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(OrderedDict))))
+    detail_aggregated_data = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(float)))))
     for file_name, date_data in data.items():
-        for date_str, categories in date_data.items():
-            date = datetime.strptime(date_str, "%Y/%m/%d")
-            year_month = date.strftime("%Y/%m")
+        for date, categories in date_data.items():
             for category, tasks in categories.items():
                 for task, task_data in tasks.items():
                     for key, val in task_data.items():
                         try:
-                            aggregated_data[file_name]['file_total'][key] = aggregated_data[file_name].get('file_total', {}).get(key, 0) + val
-                            aggregated_data[file_name][year_month]['month_total'][key] = aggregated_data[file_name][year_month].get('month_total', {}).get(key, 0) + val
-                            aggregated_data[file_name][year_month][category]['category_total'][key] = aggregated_data[file_name][year_month][category].get('category_total', {}).get(key, 0) + val
-                            aggregated_data[file_name][year_month][category][task][key] = aggregated_data[file_name][year_month][category][task].get(key, 0) + val
+                            if 'file_total' not in detail_aggregated_data[file_name]:
+                                detail_aggregated_data[file_name]['file_total'] = defaultdict(float)
+                            detail_aggregated_data[file_name]['file_total'][key] += val
+                            
+                            if 'day_total' not in detail_aggregated_data[file_name][date]:
+                                detail_aggregated_data[file_name][date]['day_total'] = defaultdict(float)
+                            detail_aggregated_data[file_name][date]['day_total'][key] += val
+                            
+                            if 'category_total' not in detail_aggregated_data[file_name][date][category]:
+                                detail_aggregated_data[file_name][date][category]['category_total'] = defaultdict(float)
+                            detail_aggregated_data[file_name][date][category]['category_total'][key] += val
+                            
+                            if task not in detail_aggregated_data[file_name][date][category]:
+                                detail_aggregated_data[file_name][date][category][task] = defaultdict(float)
+                            detail_aggregated_data[file_name][date][category][task][key] += val
                         except TypeError:
-                            print(f"Error with file_name={file_name}, year_month={year_month}, category={category}, task={task}, key={key}, val={val}")
+                            print(f"Error with file_name={file_name}, date={date}, category={category}, task={task}, key={key}, val={val}")
                             raise
-    return aggregated_data
+    return detail_aggregated_data
+
 
 def detail_json_to_html(data, depth=0):
+    """
+    Generate HTML from the detailed aggregated data dictionary.
+    """
     if isinstance(data, dict):
         if 'plan' in data.keys() or 'done' in data.keys():
             html = '<table><tbody><tr>'
