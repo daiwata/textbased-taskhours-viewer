df6a9886649a4a6ad04740f1fc261c367439794f
daiwata <watanabedai5427@gmail.com>
2023-07-17 11:59:59 +0900

    refactoring

--- app/aggregate_monthly.py
+++ app/aggregate_monthly.py
@@ -3,7 +3,7 @@ from collections import OrderedDict
 from datetime import datetime 
 
 def aggregate_monthly(data):
-    monthly_aggregated_data = defaultdict(lambda: defaultdict(lambda: defaultdict(OrderedDict)))
+    results = defaultdict(lambda: defaultdict(lambda: defaultdict(OrderedDict)))
     for file_name, date_data in data.items():
         for date_str, categories in date_data.items():
             date = datetime.strptime(date_str, "%Y/%m/%d")
@@ -12,14 +12,14 @@ def aggregate_monthly(data):
                 for task, task_data in tasks.items():
                     for key, val in task_data.items():
                         try:
-                            monthly_aggregated_data[year_month]['month_total'][key] = monthly_aggregated_data[year_month].get('month_total', {}).get(key, 0) + val
-                            monthly_aggregated_data[year_month][category]['category_total'][key] = monthly_aggregated_data[year_month][category].get('category_total', {}).get(key, 0) + val
-                            monthly_aggregated_data[year_month][category][task][key] = monthly_aggregated_data[year_month][category][task].get(key, 0) + val
+                            results[year_month]['month_total'][key] = results[year_month].get('month_total', {}).get(key, 0) + val
+                            results[year_month][category]['category_total'][key] = results[year_month][category].get('category_total', {}).get(key, 0) + val
+                            results[year_month][category][task][key] = results[year_month][category][task].get(key, 0) + val
                         except TypeError:
                             print(f"Error with year_month={year_month}, category={category}, task={task}, key={key}, val={val}")
                             raise
 
     # Sort the monthly aggregated data in reverse order by the year and month
-    monthly_aggregated_data = OrderedDict(sorted(monthly_aggregated_data.items(), reverse=True))
+    results = OrderedDict(sorted(results.items(), reverse=True))
 
-    return monthly_aggregated_data
+    return results
